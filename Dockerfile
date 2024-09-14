f46f7e2728e8ceb073b848c12780fe6ce4922b49
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-09-14 17:47:29 +0200

    fixes

--- Dockerfile
+++ Dockerfile
@@ -16,8 +16,8 @@ COPY . .
 RUN npx playwright install --with-deps
 
 # Set the environment variable
-# ENV BACKEND_URL=http://backend:4001
-# ENV FRONTEND_URL=http://frontend:8081
+ENV BACKEND_URL=http://backend:4001
+ENV FRONTEND_URL=http://frontend:8081
 
 # Create a shell script to run tests and set exit code
 RUN echo '#!/bin/sh\nnpx playwright test --reporter=list\nexit $?' > /app/run-tests.sh && chmod +x /app/run-tests.sh
--- tests/ui/login.ui.spec.ts
+++ tests/ui/login.ui.spec.ts
@@ -1,29 +1,39 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:8081';
-console.log('frontendUrl', frontendUrl);
+const frontendUrl = process.env.FRONTEND_URL || "http://localhost:8081";
+console.log("frontendUrl", frontendUrl);
 
-test('successful login', async ({ page }) => {
+test("successful login", async ({ page }) => {
   // Go to the login page
   await page.goto(frontendUrl);
+  const pageSource = await page.content();
+  console.log("Page source:", pageSource);
 
   // Fill in the login form using name attributes
-  await page.locator('input[name="username"]').fill('admin');
-  await page.locator('input[name="password"]').fill('admin');
+  await page.locator('input[name="username"]').fill("admin");
+  await page.locator('input[name="password"]').fill("admin");
 
   // Click the login button
-  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole("button", { name: "Login" }).click();
 
   // Verify successful login
-  await expect(page.getByRole('heading', { name: 'Hi Slawomir!' })).toBeVisible();
-  await expect(page.getByText("You're logged in! Congratulations :)")).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Hi Slawomir!" })
+  ).toBeVisible();
+  await expect(
+    page.getByText("You're logged in! Congratulations :)")
+  ).toBeVisible();
 
   // Verify the presence of expected elements after login
-  await expect(page.getByRole('heading', { name: 'All registered users:' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'Logout' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'Add more users' })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "All registered users:" })
+  ).toBeVisible();
+  await expect(page.getByRole("link", { name: "Logout" })).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "Add more users" })
+  ).toBeVisible();
 
   // Verify the presence of registered users
-  await expect(page.getByText('Slawomir Radzyminski')).toBeVisible();
-  await expect(page.getByText('Gosia Radzyminska')).toBeVisible();
+  await expect(page.getByText("Slawomir Radzyminski")).toBeVisible();
+  await expect(page.getByText("Gosia Radzyminska")).toBeVisible();
 });
